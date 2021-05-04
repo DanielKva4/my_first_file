e3bf3a0f6373ac6f93587a7975427fb81a88d9d4
Dasdas <mamkymal14@gmail.com>
2021-05-04 21:40:31 +0300

    Testing

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 import re
 
-# print(re.match("^[HBMP][HBMP]\d{7}$", 'PB1234567') is not None)
-#
-# print(re.match('^(Привет|Добрый день|Салют)\sдорогой юзер$', 'Привет дорогой юзер') is not None)
-#
-# print(re.match('^\d\s(\+|\/|\*)\s\d$', '5 * 5') is not None)
-#
-login = 'give_me_spam@gmail.com'
-# print(re.match('^(.+)@gmail\.com$', login).groups())
-# print(re.match('^a\d+c$', 'a777c') is not None)
-
-def login():
-    x = input('get_login: ')
-    if re.match('^.+@gmail\.com$', x) is not None:
-        return re.match('^(.+)@gmail\.com$', x).groups()
-    else:
-        raise Exception("Ты лох")
-# print(login())
 
 def fn_5(x):
     if re.match('^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}$', x) is not None:
